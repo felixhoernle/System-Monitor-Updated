3b2ad2ba488014dc7b99839887d7c17a668685f9
Felix Hörnle <felix.hoernle@gmail.com>
2024-01-02 11:43:11 +0000

    Updated format.cpp to consider C-string terminating null character

--- src/format.cpp
+++ src/format.cpp
@@ -7,16 +7,16 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 string Format::ElapsedTime(long seconds) {
-  int hours = seconds / 3600;
+  long hours = seconds / 3600;
   seconds -= hours * 3600;
 
-  int minutes = seconds / 60;
+  long minutes = seconds / 60;
   seconds -= minutes * 60;
 
   // Create a C-string, print elapsed time in output format to it and then
   // convert it to a std::string
-  char c[8];
-  sprintf(c, "%02d:%02d:%02d", hours, minutes, seconds);
+  char c[9]; // C-string has to have space for time plus terminating null character
+  snprintf(c, 9, "%02ld:%02ld:%02ld", hours, minutes, seconds);
   string s(c);
 
   return s;
